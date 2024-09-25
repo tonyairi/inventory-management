9464343a444ad0edc598ab12dbc5c93debcfba05
Tony Peng <lp1dxing@gmail.com>
2024-09-25 14:43:06 +1000

    update image url with s3 public ip

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -67,13 +67,13 @@ const Navbar = () => {
           <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
           <div className="flex items-center gap-3 cursor-pointer">
             <Image
-              src="https://s3-inventorymanagement.s3.us-east-2.amazonaws.com/profile.jpg"
+              src="https://s3-inventory-management-app.s3.ap-southeast-2.amazonaws.com/profile.jpg"
               alt="Profile"
               width={50}
               height={50}
               className="rounded-full h-full object-cover"
             />
-            <span className="font-semibold">Ed Roh</span>
+            <span className="font-semibold">Tony Peng</span>
           </div>
         </div>
         <Link href="/settings">
